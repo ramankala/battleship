153e64020897660086227d9956b367bac9738cf6
Raman <rs.kala@hotmail.com>
2022-02-22 23:09:00 -0500

    Function isSunk completed
    Created logic which checks if all positions on the ships are hit, thus
    returning true, otherwise false if all positions are not hit.

--- src/shipFactory.js
+++ src/shipFactory.js
@@ -4,9 +4,28 @@ const shipFactory = (length) => {
 
     const hit = (pos) => ship[pos] = 'hit';
 
+    const isSunk = () => {
+        let count = 0;
+        let sunk = false;
+
+        ship.forEach(function(item){
+            if (item === 'hit'){
+                count += 1;
+            }
+        });
+        if (count === length) {
+            sunk = true;
+        } else {
+            sunk = false;
+        }
+
+        return sunk;
+    };
+
     return {
         length,
-        hit
+        hit,
+        isSunk,
     }
 };
 
