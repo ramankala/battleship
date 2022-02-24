1d32b5425ecc367e6ab3c75e3609649931cb0303
Raman <rs.kala@hotmail.com>
2022-02-24 02:13:29 -0500

    placeShip function created
    Created logic for placing ships on the gameboard, the logic for testing
    it is shaky though.  placeShip function still needs more work.

--- src/gameBoardFactory.js
+++ src/gameBoardFactory.js
@@ -1,10 +1,31 @@
+import { shipFactory } from './shipFactory';
 
 const gameBoardFactory = () => {
     let gameBoard = [];
     const board = () => {
         for (let i = 0; i < 10; i++){
             for (let j = 0; j < 10; j++){
-                gameBoard.push({hasShip: false, isShot: false});
+                gameBoard.push([{hasShip: false, isShot: false}]);
+            };
+        };
+        // return gameBoard.length;
+    };
+
+    const placeShip = (x, y, shipLength) => {
+        // should be calling ship factory here
+        for (let i = 0; i < x ; i++){
+            for (let j = y; j < y + shipLength; j++){
+                gameBoard.hasShip = true;
+                return gameBoard.hasShip;
+            }
+        }
+    }
+
+    const viewShip = (x, y, shipLength) => {
+        placeShip(x, y, shipLength);
+        for (let i = 0; i < x; i++){
+            for (let j = y; j < y + shipLength; j++){
+                return gameBoard.hasShip;
             };
         };
     };
@@ -12,6 +33,8 @@ const gameBoardFactory = () => {
     return {
         gameBoard,
         board,
+        placeShip,
+        viewShip,
     }
 };
 
--- src/gameBoardFactory.test.js
+++ src/gameBoardFactory.test.js
@@ -1,8 +1,14 @@
 import { gameBoardFactory } from './gameBoardFactory';
 
-test('Test the gameboard has the right number of cells', () => {
+test.skip('Test the gameboard has the right number of cells', () => {
     expect(gameBoardFactory().board()).toBe(100);
 });
-test.skip('Testing that gameboard places ships are correct coordinates', () => {
-    expect(gameBoardFactory.placeShip(2, 2, 4)).toBe(true);
+test('Testing that gameboard places ships are correct coordinates', () => {
+    expect(gameBoardFactory().placeShip(2, 2, 4)).toBe(true);
+});
+test('Testing viewShip works', () => {
+    expect(gameBoardFactory().viewShip(2, 2, 4)).toBe(true);
+});
+test('Testing that gameboard places ships in correct positions', () => {
+    expect(gameBoardFactory().placeShip(2, 2, 4)).toBe(gameBoardFactory().viewShip(2, 2, 4));
 });
