5d538d3b29c83873368ddbc171d478d61b19b003
Raman <rs.kala@hotmail.com>
2022-02-25 01:14:17 -0500

    Gameboard factory refactored
    Changed the board logic so that it's simpler to access elements and push
    elements to.  Changed logic for placing ships on the gameboard, now
    toggles the property on an object each slot the gameboard has.

--- src/gameBoardFactory.js
+++ src/gameBoardFactory.js
@@ -1,25 +1,46 @@
 import { shipFactory } from './shipFactory';
 
 const gameBoardFactory = () => {
-    let gameBoard = [];
+    let gameBoard = [
+        (["", "", "", "", "", "", "", "", "", ""]),
+        (["", "", "", "", "", "", "", "", "", ""]),
+        (["", "", "", "", "", "", "", "", "", ""]),
+        (["", "", "", "", "", "", "", "", "", ""]),
+        (["", "", "", "", "", "", "", "", "", ""]),
+        (["", "", "", "", "", "", "", "", "", ""]),
+        (["", "", "", "", "", "", "", "", "", ""]),
+        (["", "", "", "", "", "", "", "", "", ""]),
+        (["", "", "", "", "", "", "", "", "", ""]),
+        (["", "", "", "", "", "", "", "", "", ""]),
+    ];
     const board = () => {
         for (let i = 0; i < 10; i++){
             for (let j = 0; j < 10; j++){
-                gameBoard.push([{hasShip: false, isShot: false}]);
+                gameBoard[i][j] = ({hasShip: false, isShot: false});
             };
         };
-        // return gameBoard.length;
+        return gameBoard;
     };
+
 
     const placeShip = (x, y, shipLength) => {
         // should be calling ship factory here
-        for (let i = 0; i < x ; i++){
-            for (let j = y; j < y + shipLength; j++){
-                gameBoard.hasShip = true;
-                return gameBoard.hasShip;
-            }
+        board();
+        for (let i = 0; i < shipLength; i++){
+            gameBoard[x][y].hasShip = true;
+            y += 1;
         }
+        return gameBoard;
     }
+
+    const receiveAttack = (x, y) => {
+        placeShip(x, y, 4);
+        if (gameBoard[x][y].hasShip === true){
+            return 'Hit';
+        } else {
+            return 'Miss';
+        }
+    };
 
     const viewShip = (x, y, shipLength) => {
         placeShip(x, y, shipLength);
@@ -34,7 +55,9 @@ const gameBoardFactory = () => {
         gameBoard,
         board,
         placeShip,
+        receiveAttack,
         viewShip,
+        
     }
 };
 
--- src/gameBoardFactory.test.js
+++ src/gameBoardFactory.test.js
@@ -1,14 +1,48 @@
 import { gameBoardFactory } from './gameBoardFactory';
 
-test.skip('Test the gameboard has the right number of cells', () => {
-    expect(gameBoardFactory().board()).toBe(100);
+test('Test the gameboard has the right number of cells', () => {
+    expect(gameBoardFactory().gameBoard).toEqual([
+        (["", "", "", "", "", "", "", "", "", ""]),
+        (["", "", "", "", "", "", "", "", "", ""]),
+        (["", "", "", "", "", "", "", "", "", ""]),
+        (["", "", "", "", "", "", "", "", "", ""]),
+        (["", "", "", "", "", "", "", "", "", ""]),
+        (["", "", "", "", "", "", "", "", "", ""]),
+        (["", "", "", "", "", "", "", "", "", ""]),
+        (["", "", "", "", "", "", "", "", "", ""]),
+        (["", "", "", "", "", "", "", "", "", ""]),
+        (["", "", "", "", "", "", "", "", "", ""]),
+    ]);
+});
+test('Testing objects being pushed to each slot in gameboard', () => {
+    expect(gameBoardFactory().board()).toEqual([
+        ([{hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}]),
+        ([{hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}]),
+        ([{hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}]),
+        ([{hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}]),
+        ([{hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}]),
+        ([{hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}]),
+        ([{hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}]),
+        ([{hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}]),
+        ([{hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}]),
+        ([{hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}]),
+    ])
 });
 test('Testing that gameboard places ships are correct coordinates', () => {
-    expect(gameBoardFactory().placeShip(2, 2, 4)).toBe(true);
+    expect(gameBoardFactory().placeShip(2, 2, 4)).toEqual([
+        ([{hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}]),
+        ([{hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}]),
+        ([{hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: true, isShot: false}, {hasShip: true, isShot: false}, {hasShip: true, isShot: false}, {hasShip: true, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}]),
+        ([{hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}]),
+        ([{hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}]),
+        ([{hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}]),
+        ([{hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}]),
+        ([{hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}]),
+        ([{hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}]),
+        ([{hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}, {hasShip: false, isShot: false}]),
+    ]);
 });
-test('Testing viewShip works', () => {
-    expect(gameBoardFactory().viewShip(2, 2, 4)).toBe(true);
-});
-test('Testing that gameboard places ships in correct positions', () => {
-    expect(gameBoardFactory().placeShip(2, 2, 4)).toBe(gameBoardFactory().viewShip(2, 2, 4));
+
+test.skip('Testing receiveAttack takes coordinates and misses shot', () => {
+    expect(gameBoardFactory().receiveAttack(2, 2)).toBe('Miss');
 });
